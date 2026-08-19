cf9eeff0f68416331c923637071ef225434047f3
Kyle Yingjie <kyleyingjie@users.noreply.github.com>
2026-08-19 14:17:15 +0800

    Add final colored image to Wind Character Design

--- site-data.js
+++ site-data.js
@@ -272,7 +272,8 @@ window.PORTFOLIO_PUBLISHED_DATA = {
         "assets/projects/wind-character-design/wind-character-design-01.jpg",
         "assets/projects/wind-character-design/wind-character-design-02.jpg",
         "assets/projects/wind-character-design/wind-character-design-03.jpg",
-        "assets/projects/wind-character-design/wind-character-design-04.jpg"
+        "assets/projects/wind-character-design/wind-character-design-04.jpg",
+        "assets/projects/wind-character-design/wind-character-design-05.jpg"
       ]
     }
   ]
